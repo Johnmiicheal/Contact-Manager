fc154b79dfb95ccfe862be5e1a1e55a58b88f1f5
Elijah Johnmicheal <69642932+Johnmiicheal@users.noreply.github.com>
2021-01-07 17:10:03 +0100

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
+
+class Contact extends Component {
+  showClick() {
+    console.log("I clicked this");
+  }
+  render() {
+    const { name, email, phone } = this.props;
+
+    return (
+      <div className="card card-body mb-3">
+        <h4>
+          {name} <FontAwesomeIcon icon="sort-down" />
+        </h4>
+        <ul className="list-group">
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Tel: {phone}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+};
+
+export default Contact;
