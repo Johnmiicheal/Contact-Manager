358603f8fbfb19bdf8b16f2a5a2e61bfd3eddcbb
Elijah Johnmicheal <69642932+Johnmiicheal@users.noreply.github.com>
2021-01-07 22:15:52 +0100

    Update Icons
    Icons are ready to be clicked.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import PropTypes from "prop-types";
 
 class Contact extends Component {
@@ -12,7 +13,7 @@ class Contact extends Component {
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name} <FontAwesomeIcon icon="sort-down" />
+          {name} <FontAwesomeIcon icon="angle-down" />
         </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
